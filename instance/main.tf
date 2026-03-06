9ca07ddc4d6a07eb5f0c55004203f32afae09a2c
afuwape2024 <afuwape2024@gmail.com>
2026-03-06 01:26:24 +0000

    first

--- instance/main.tf
+++ instance/main.tf
@@ -10,3 +10,17 @@ resource "aws_instance" "web_server" {
   tags = var.mandatory_tags
 
 }
+
+resource "aws_instance" "jenkin_server" {
+  count = 1
+  ami     = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.public_subnet
+  vpc_security_group_ids = [var.tier2_public_sg]
+  user_data = file("${path.module}/jenkin.sh")
+
+  tags = {
+    Name = "jenkins_server"
+  }
+
+}
